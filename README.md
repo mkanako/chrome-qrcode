76c31303a252cae9bdded0aca2964f4083879fec
czt <617273720@qq.com>
2017-07-02 02:51:49 +0800

    new version

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-# chrome扩展-二维码生成器
+# chrome扩展 - 二维码生成器
 
-![img](https://github.com/mkanako/chrome_extension_qrcode/blob/master/Screenshot.png?raw=true)
+![img](https://github.com/mkanako/chrome-qrcode/blob/master/Screenshot.png?raw=true)
 
 ## Features
-* 默认生成当前页面二维码，可在输入框修改生成其他字符串的二维码
+* 默认生成当前页面二维码，点击二维码进入编辑
 
-* 二维码大小根据长度自动调整
+* 二维码大小根据文本长度自动调整
 
 * 右键链接或者选择文本右键也可生成二维码
 
-##Dependences
+## Dependences
 [qrcodejs](https://github.com/davidshimjs/qrcodejs)
 
-[vue2](https://github.com/vuejs/vue)
-
 ## License
-[MIT](https://github.com/mkanako/chrome_extension_qrcode/blob/master/LICENSE)
+[MIT](https://github.com/mkanako/chrome-qrcode/blob/master/LICENSE)
--- js/app.js
+++ js/app.js
@@ -1,53 +1,57 @@
-app = new Vue({
-    el: '#app',
-    data: {
-        qr_str: '',
-        qr_obj: null
-    },
-    mounted: function() {
-        this.qr_obj = new QRCode(document.getElementById("qr"), {
-            correctLevel: QRCode.CorrectLevel.L,
-        });
-        var vm = this;
-        chrome.tabs.query({ active: true }, function(tab) {
-            var url = tab[0].url;
-            if (url) {
-                if (RegExp('^chrome-extension:\/\/' + chrome.i18n.getMessage("@@extension_id")).test(url)) {
-                    chrome.runtime.sendMessage({ ready: 1 }, function(response) {
-                        vm.qr_str = response.str;
-                        vm.make();
-                    });
-                    chrome.runtime.onMessage.addListener(
-                        function(request, sender, sendResponse) {
-                            if (request.str) {
-                                vm.qr_str = request.str;
-                                vm.make();
-                            }
-                        });
-                } else {
-                    vm.qr_str = url;
-                    vm.make();
-                }
+window.onload = function() {
+    qr_obj = new QRCode(document.getElementById("qr"), {
+        correctLevel: QRCode.CorrectLevel.L
+    })
+    qr_show = document.getElementById('qr-show')
+    qr_edit = document.getElementById('qr-edit')
+    textarea = document.getElementsByTagName('textarea')[0]
+    qr_str = ''
+    chrome.tabs.query({ active: true }, function(tab) {
+        var url = tab[0].url
+        if (url) {
+            if (RegExp('^chrome-extension:\/\/' + chrome.i18n.getMessage("@@extension_id")).test(url)) {
+                chrome.runtime.sendMessage({ ready: 1 }, function(response) {
+                    if (response && response.str) {
+                        qr_str = response.str
+                        make()
+                    }
+                })
+                chrome.runtime.onMessage.addListener(
+                    function(request, sender, sendResponse) {
+                        if (request && request.str) {
+                            qr_str = request.str
+                            make()
+                        }
+                    })
+            } else {
+                qr_str = url
+                make()
             }
-        })
-    },
-    methods: {
-        make: function(event) {
-            var width;
-            var minWidth = 128;
-            var maxWidth = 360;
-            var rate = 2;
-            this.qr_str = this.qr_str.toString().trim();
-            if (!this.qr_str) return;
-            width = this.qr_str.length * rate;
-            if (width < minWidth) {
-                width = minWidth
-            } else if (width > maxWidth) {
-                width = maxWidth
-            }
-            this.qr_obj._oDrawing._elCanvas.height = this.qr_obj._oDrawing._elCanvas.width = width;
-            this.qr_obj._htOption.width = this.qr_obj._htOption.height = width;
-            this.qr_obj.makeCode(this.qr_str);
         }
+    })
+
+    function make() {
+        var str = qr_str.trim()
+        if (!str) return
+        textarea.value = str
+        var width = Math.max(200, qr_str.length);
+        width = Math.min(width, 400);
+        qr_obj._oDrawing._elCanvas.height = qr_obj._oDrawing._elCanvas.width = width;
+        qr_obj._htOption.width = qr_obj._htOption.height = width;
+        qr_obj.makeCode(str);
+        qr_show.style.display = 'block'
+        qr_edit.style.display = 'none'
     }
-})
+    document.getElementById('confirm').addEventListener('click', function() {
+        qr_str = textarea.value
+        make()
+    })
+    document.getElementById('cancle').addEventListener('click', function() {
+        qr_show.style.display = 'block'
+        qr_edit.style.display = 'none'
+    })
+    document.getElementById('qr').addEventListener('click', function() {
+        qr_show.style.display = 'none'
+        qr_edit.style.display = 'block'
+    })
+}
